e75c14e3330c0e0d40dc9768dc60439ccc3cf6cc
Phlogistonny <pogosyan.chris@gmail.com>
2018-10-03 22:43:33 +0300

    new header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.png';
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <div className="App-header-menu">
+            <ul>
+              <li className="App-header-btn App-header-btn-left"><a href="#">Каталог</a></li>
+              <li  className="App-header-btn App-header-btn-right"><a href="#">Личный кабинет</a></li>
+            </ul>
+          </div>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-promo">
+        </div>
       </div>
     );
   }
